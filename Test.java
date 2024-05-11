c485fa54b7f2634806a7769e1f46446acb5169e2
niiang <137999923+niiang@users.noreply.github.com>
2024-05-12 00:16:54 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,17 +1,15 @@
 package theendwork;
 
 public class Test extends TransferStations  {
-    //private <identifyTransferStations>;
     public static void main(String[] args) {
-        TransferStations transferStations=new TransferStations();
-        transferStations.print();
+        //TransferStations transferStations=new TransferStations();
+        //transferStations.print();
         //以上是实验一
-
         //下面是实验二
         String startStation = "华中科技大学";
-        int n = 3;
+        int n = 2;
         Findmindistance find = new Findmindistance();
-        find.printstation(startStation, n);
+        find.printminditance(startStation, n);
     }
     
 }
